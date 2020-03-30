cbf20747a449453448121c95b703228d7947d8e5
Caio Godoy <caiogod@Lecutech.local>
2020-03-30 13:16:31 -0300

    Changing procfile

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: 'NjkzNDcxNDYwMzA3NTAxMDY3.Xn-JdQ.zEtxZzsdiOtEwA8hqodi3xFFz1w',
+  token: process.env.token,
   generalChannel: '238835694779367424',
   recChannel: '693463884790759444'
 }
